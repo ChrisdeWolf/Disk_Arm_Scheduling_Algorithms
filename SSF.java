e560c6bcf5ef273ec3152a0f15e4edd4d0bb3646
Christopher deWolf <30628193+ChrisdeWolf@users.noreply.github.com>
2018-04-04 16:47:21 -0400

    Update SSF.java

--- SSF.java
+++ SSF.java
@@ -12,32 +12,32 @@ public class SSF
 {
    int run(int[] Accesses, int size, int start, boolean chat)
    {    
-	   	System.out.print("SET 2 SSF");
+	   System.out.print("SET 2 SSF");
 		
-	    int count=0;
-		int[] distances = new int[size];
-	    int[] ssfrequests = new int[size];			//create a new array with accesses data to modify
-	    for( int i=0; i < ssfrequests.length; i++)  //fill new array with accesses data
+	    int count = 0;
+	    int[] distances = new int[size];
+	    int[] ssfrequests = new int[size];		  //create a new array for accesses data to modify later
+	    for( int i = 0; i < ssfrequests.length; i++)  //fill new array with accesses data
 	    	ssfrequests[i] = Accesses[i];
-	    int movement;							//variable keeps track of distance moved
+	    int movement;				  //keeps track of total distance moved
 	    int previous = start;
 	    int shortest;
 	    System.out.print("\nCylinders: " + start + " ");
    	    
-	    for( int i=0; i < ssfrequests.length; i++)			//finding the smallest distance and sort
+	    for( int i = 0; i < ssfrequests.length; i++)	  //finding the smallest distance and sort
 	    {   
-	    	shortest = i;				//current value being looked at
-	    	movement = Math.abs(previous - ssfrequests[i]);  //distance we are comparing
+	    	shortest = i;					  //current request being looked at
+	    	movement = Math.abs(previous - ssfrequests[i]);   //distance we are comparing
 	    	
-	    	for( int k = i+1; k < ssfrequests.length; k++)		//looks at next pieces of data
+	    	for( int k = i+1; k < ssfrequests.length; k++)		//looks at all the next requests
 	    	{
-	    		if( Math.abs( previous - ssfrequests[k]) < movement)	//if this next piece has shorter distance
-	    		{														//then this is now movement and the current
+	    		if( Math.abs( previous - ssfrequests[k]) < movement)	//if this next piece has a shorter distance
+	    		{							//then it is the new shortest
 	    			movement = ( Math.abs( previous - ssfrequests[k]));
 	    			shortest = k;
 	    		}
 	    	}
-	    									//swapping the distances
+	    						//swapping the distances
 	    	int tmp = ssfrequests[i];
 	    	ssfrequests[i] = ssfrequests[shortest];
 	    	ssfrequests[shortest] = tmp;
@@ -46,23 +46,24 @@ public class SSF
     
 	    previous = start;
     
-	    for( int i = 0; i < ssfrequests.length; i++)
-		{
+	    for( int i = 0; i < ssfrequests.length; i++)	//calculating and storing movements
+	    {
 	    	movement = Math.abs( ssfrequests[i] - previous);
 	    	System.out.print( "\t " + ssfrequests[i]);
 			
-			distances[i] = movement;		//fills array of distance moved for printing later
-			count += movement;
-			previous = ssfrequests[i];
-		}	    
-	    System.out.print( "\nDistances:   ");
-        for( int i = 0; i < ssfrequests.length; i++)
-	 	{
-        	System.out.print( "\t " + distances[i]);	
-	 	}	    
+		distances[i] = movement;		//fills array of distance moved for printing later
+		count += movement;
+		previous = ssfrequests[i];
+	    }
+	   
+	    System.out.print( "\nDistances:   ");		//prints distances
+	    for( int i = 0; i < ssfrequests.length; i++)
+	    { 
+            	System.out.print( "\t " + distances[i]);	
+	    }	    
 	    if( chat) 
 	    	System.out.println("SSF Total distance is " + count);
-	    if( !chat) 
+	    if( !chat)
 	    	System.out.print("\n");
 	    
 	   	return count;
